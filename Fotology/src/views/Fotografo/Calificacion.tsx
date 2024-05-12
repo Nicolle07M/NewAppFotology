6ee5bb7c7d5508f89bf3942e4f30acc17ca14d51
Nicolle <marqueznicolle421@gmail.com>
2024-05-11 20:03:55 -0500

    feat(FrontEnd): separating calificacion styles

--- Fotology/src/views/Fotografo/Calificacion.tsx
+++ Fotology/src/views/Fotografo/Calificacion.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; // Importa el icono de Ionicons
-
+import styles from './GlobalStyles/CalificacionStyles'
 const CalificacionScreen = () => {
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState('');
@@ -58,48 +58,5 @@ const CalificacionScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  ratingContainer: {
-    flexDirection: 'row',
-    marginBottom: 20,
-  },
-  ratingButton: {
-    marginRight: 10,
-  },
-  selected: {
-    opacity: 1,
-  },
-  commentInput: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 20,
-    width: '100%',
-    minHeight: 100,
-  },
-  submitButton: {
-    backgroundColor: '#FFD700',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  submitButtonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-});
 
 export default CalificacionScreen;
