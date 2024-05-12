bfabf46e647eb5730114dc6479099d09a0bfc0e3
AlejandraPuentes <147003574+Aleja49@users.noreply.github.com>
2024-05-11 19:49:53 -0500

    feat(FrontEnd): add views portafolio and categorias, separing styles

--- Fotology/src/views/Fotografo/Categorias.tsx
+++ Fotology/src/views/Fotografo/Categorias.tsx
@@ -1,33 +1,21 @@
-import styles from './GlobalStyles/CategoriasStyles';
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, ViewStyle } from 'react-native';
 import { Dimensions } from 'react-native';
+import CategoriasStyles from './GlobalStyles/CategoriasStyles';
 
 const windowWidth = Dimensions.get('window').width;
-const contentWidth = windowWidth - 60;
-const headerHeight = 100; 
-const headerWidth = windowWidth; 
+const headerHeight = 100;
+const headerWidth = windowWidth;
 
 export const CategoriasScreen = () => {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-  const [, updateState] = useState<undefined | (() => void)>(undefined); 
-  const [showList, setShowList] = useState(false); 
 
-  const forceUpdate = () => updateState(undefined); 
-
-  const handleSymbolPress = () => {
-    console.log('Botón del símbolo presionado');
-    setShowList(!showList); 
-  };
   const handleCategoryButtonPress = (text: string) => {
     console.log(`Botón de ${text} presionado`);
     setSelectedCategory(text);
-    forceUpdate();
   };
-  const handleBottomButtonPress = () => {
-    console.log('Botón de abajo presionado');
-  };
+
   const categories = [
     'Paisajes',
     'Retratos',
@@ -36,47 +24,37 @@ export const CategoriasScreen = () => {
     'Viajes',
     'Eventos',
   ];
-  const containerStyle: ViewStyle = {
-    ...styles.container,
-    paddingHorizontal: 20,
-  };
+  
   return (
-    <View style={containerStyle}>
+    <View style={CategoriasStyles.container}>
       <StatusBar style="auto" />
-      <View style={[styles.header, { height: headerHeight, width: headerWidth }]}>
-        <Text style={styles.headerText}>Portafolio</Text>
+      <View style={[CategoriasStyles.header, { height: headerHeight, width: headerWidth }]}>
+        <Text style={CategoriasStyles.headerText}>Portafolio</Text>
       </View>
-      <View style={styles.content}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Seleccionar la categoría</Text>
-          <TouchableOpacity onPress={handleSymbolPress}>
-            <Text style={styles.symbolText}>▼</Text>
-          </TouchableOpacity>
+      <View style={CategoriasStyles.content}>
+        {/* Mostrar la lista siempre */}
+        <View style={CategoriasStyles.listContainer}>
+          {categories.map((category, index) => (
+            <View key={index} style={CategoriasStyles.listItemContainer}>
+              <Text style={CategoriasStyles.listItem}>
+                {category.split('').join(' ')}
+              </Text>
+              <TouchableOpacity onPress={() => handleCategoryButtonPress(category)}>
+                <View style={[CategoriasStyles.categoryButton, selectedCategory === category && CategoriasStyles.categoryButtonSelected]}>
+                  {selectedCategory === category && <Text style={CategoriasStyles.checkmark}>✓</Text>}
+                </View>
+              </TouchableOpacity>
+            </View>
+          ))}
         </View>
-        {/* Mostrar la lista solo si showList es true */}
-        {showList && (
-          <View style={styles.listContainer}>
-            {categories.map((category, index) => (
-              <View key={index} style={styles.listItemContainer}>
-                <Text style={styles.listItem}>
-                  {category.split('').join(' ')} 
-                </Text>
-                <TouchableOpacity onPress={() => handleCategoryButtonPress(category)}>
-                  <View style={[styles.categoryButton, selectedCategory === category && styles.categoryButtonSelected]}>
-                    {selectedCategory === category && <Text style={styles.checkmark}>✓</Text>}
-                  </View>
-                </TouchableOpacity>
-              </View>
-            ))}
-          </View>
-        )}
         {selectedCategory && (
-          <TouchableOpacity style={styles.bottomButton} onPress={handleBottomButtonPress}>
-            <Text style={styles.bottomButtonText}>Confirmar selección</Text>
+          <TouchableOpacity style={CategoriasStyles.bottomButton} onPress={() => setSelectedCategory(null)}>
+            <Text style={CategoriasStyles.bottomButtonText}>Confirmar selección</Text>
           </TouchableOpacity>
         )}
       </View>
     </View>
   );
 }
-export default CategoriasScreen; 
+
+export default CategoriasScreen;
--- Fotology/src/views/Fotografo/GlobalStyles/CategoriasStyles.tsx
+++ Fotology/src/views/Fotografo/GlobalStyles/CategoriasStyles.tsx
@@ -1,98 +1,85 @@
 import { StyleSheet } from "react-native";
-const CategoriasStyles= StyleSheet.create({
 
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      paddingTop: 30,
-    },
-    header: {
-      backgroundColor: 'orange',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    headerText: {
-      fontFamily: 'Homenaje',
-      fontSize: 30,
-      color: 'white',
-    },
-    content: {
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 20,
-    },
-    button: {
-      backgroundColor: 'orange',
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderRadius: 25,
-      paddingVertical: 12,
-      paddingHorizontal: 20,
-      marginBottom: 20,
-    },
-    buttonText: {
-      fontFamily: 'Homenaje',
-      fontSize: 20,
-      color: 'white',
-      marginRight: 5,
-    },
-    symbolText: {
-      fontFamily: 'Homenaje',
-      fontSize: 20,
-      color: 'white',
-    },
-    listContainer: {
-      width: '100%',
-      alignItems: 'flex-start',
-    },
-    listItemContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: 'white',
-      marginLeft: 10,
-      marginRight: 10,
-    },
-    listItem: {
-      fontFamily: 'Homenaje',
-      fontSize: 18,
-      color: '#333',
-      paddingVertical: 25,
-    },
-    categoryButton: {
-      width: 40,
-      height: 40,
-      backgroundColor: 'transparent',
-      borderRadius: 20,
-      borderWidth: 2,
-      borderColor: 'orange',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    categoryButtonSelected: {
-      backgroundColor: 'orange',
-    },
-    checkmark: {
-      color: 'white',
-      fontSize: 20,
-    },
-    bottomButton: {
-      backgroundColor: 'orange',
-      borderRadius: 25,
-      paddingVertical: 12,
-      paddingHorizontal: 20,
-      marginTop: 20,
-    },
-    bottomButtonText: {
-      fontFamily: 'Homenaje',
-      fontSize: 20,
-      color: 'white',
-    },
-  });
-  export default CategoriasStyles;
+const CategoriasStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 30,
+  },
+  header: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontFamily: 'Homenaje',
+    fontSize: 30,
+    color: 'white',
+  },
+  content: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  listContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+  },
+  listItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray', // Añade un borde inferior
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  listItem: {
+    fontFamily: 'Homenaje',
+    fontSize: 18,
+    color: '#333',
+    paddingVertical: 25,
+    flex: 1, // Hace que el texto ocupe todo el espacio disponible
+  },
+  categoryButtonContainer: {
+    flexDirection: 'row', // Alinea los botones horizontalmente
+  },
+  categoryButton: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'transparent',
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 20, // Añade un margen derecho entre botones
+  },
+  categoryButtonSelected: {
+    backgroundColor: 'orange',
+  },
+  checkmark: {
+    color: 'white',
+    fontSize: 20,
+  },
+  bottomButton: {
+    backgroundColor: 'orange',
+    borderRadius: 25,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    marginTop: 20,
+    alignSelf: 'center', // Centra el botón
+  },
+  bottomButtonText: {
+    fontFamily: 'Homenaje',
+    fontSize: 20,
+    color: 'white',
+  },
+});
+
+export default CategoriasStyles;
