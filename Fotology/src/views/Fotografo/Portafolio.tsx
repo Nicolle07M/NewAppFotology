894f95382bac7d11abb01d71987d49e12be37ca2
Nicolle <marqueznicolle421@gmail.com>
2024-05-14 19:31:50 -0500

    feat(FrontEnd): update view portafolio

--- Fotology/src/views/Fotografo/Portafolio.tsx
+++ Fotology/src/views/Fotografo/Portafolio.tsx
@@ -43,7 +43,7 @@ const PortafolioScreen = () => {
         >
           <View style={PortafolioStyles.overlay}>
             <View style={PortafolioStyles.textContainer}>
-              <Text style={PortafolioStyles.text}>Categorias</Text>
+              <Text style={PortafolioStyles.text}>Portafolio</Text>
               <Text style={PortafolioStyles.text2}>¡Crea tus categorias y publica!</Text>
             </View>
           </View>
