4d47471346c975344717fbf26f57fd1d9c602a3d
Nicolle <marqueznicolle421@gmail.com>
2024-05-23 00:17:45 -0500

    (FrontEnd): add hook

--- Fotology/src/Presentation/views/Home/viewModel.tsx
+++ Fotology/src/Presentation/views/Home/viewModel.tsx
@@ -2,75 +2,101 @@ import React, { useState, useEffect } from 'react';
 import { LoginAuthUseCase } from '../../../Domain/useCases/auth/LoginAuth';
 import { GetUserLocalUseCase } from '../../../Domain/useCases/userLocal/GetUserLocal';
 import { SaveUserLocalUseCase } from '../../../Domain/useCases/userLocal/SaveUserLocal';
+import { useUserLocal } from '../../hooks/useUserLocal';
 const HomeViewModel = () => {
+
   const [errorMessage, setErrorMessage] = useState('');
-  const [values, setValues] = useState({
-    email: '',
-    password: ''
-  });
-
+  
+  const [values, setValues] = useState(
+  
+  {
+  
+  email: '',
+  
+  password: ''
+  
+  }
+  
+  );
+  
+  const { fotografo, getUserSession } = useUserLocal();
+  
+  console.log('Usuario: ' + JSON.stringify(fotografo));
+  
   useEffect(() => { //Se ejecuta cuando se instancia el viewModel
-
-    getUserSession();
-    
-    }, []);
-    
-    const getUserSession = async () => {
-    
-    const user = await GetUserLocalUseCase();
-    
-    console.log('Usuario Sesion: ' + JSON.stringify(user));
-    
-    }
-
+  
+  getUserSession();
+  
+  }, []);
+  
   const onChange = (property: string, value: any) => {
-    setValues({ ...values, [property]: value });
-  };
-
+  
+  setValues({...values, [property]: value});
+  
+  }
+  
   const login = async () => {
-
-    if (isValidForm()) {
-    
-    const response = await LoginAuthUseCase(values.email, values.password);
-    
-    console.log('Respuesta: ' + JSON.stringify(response));
-    
-    if(!response.success) {
-    
-    setErrorMessage(response.message);
-    
-    }
-    
-    else {
-    
-    await SaveUserLocalUseCase(response.data);
-    
-    }
-    
-    }
-    
-    };
-
+  
+  if (isValidForm()) {
+  
+  const response = await LoginAuthUseCase(values.email, values.password);
+  
+  console.log('Respuesta: ' + JSON.stringify(response));
+  
+  if(!response.success) {
+  
+  setErrorMessage(response.message);
+  
+  }
+  
+  else {
+  
+  await SaveUserLocalUseCase(response.data);
+  
+  getUserSession();
+  
+  }
+  
+  }
+  
+  };
+  
   const isValidForm = () => {
-    if (values.email === '') {
-      setErrorMessage('El email es requerido');
-      return false;
-    }
-
-    if (values.password === '') {
-      setErrorMessage('La contraseña es requerida');
-      return false;
-    }
-
-    return true;
-  };
-
+  
+  if (values.email === '') {
+  
+  setErrorMessage('El email es requerido');
+  
+  return false;
+  
+  }
+  
+  if (values.password === '') {
+  
+  setErrorMessage('La contraseña es requerida');
+  
+  return false;
+  
+  }
+  
+  return true;
+  
+  }
+  
   return {
-    ...values,
-    onChange,
-    login,
-    errorMessage
-  };
-};
-
-export default HomeViewModel;
+  
+  ...values,
+  
+  fotografo,
+  
+  onChange,
+  
+  login,
+  
+  errorMessage
+  
+  }
+  
+  }
+  
+  export default HomeViewModel;
